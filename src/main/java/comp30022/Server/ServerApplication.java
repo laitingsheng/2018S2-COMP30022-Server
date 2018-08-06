5087e9b9ef8695997d5471f2f0ce8692e9124e51
Tinson <laitingsheng@vip.qq.com>
2018-08-06 22:55:57 +1000

    Created Spring Boot
    Spring Boot project initialised from https://start.spring.io/

---
+++ src/main/java/comp30022/Server/ServerApplication.java
@@ -0,0 +1,12 @@
+package comp30022.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
