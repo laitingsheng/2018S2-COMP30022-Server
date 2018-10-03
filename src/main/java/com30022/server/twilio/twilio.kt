a00a6db49c6378cd6811ea65b0886418f65831c0
Tinson <laitingsheng@vip.qq.com>
2018-10-03 12:11:13 +1000

    Finalise ChatToken response

--- src/main/java/com30022/server/twilio/twilio.kt
+++ src/main/java/com30022/server/twilio/twilio.kt
@@ -1,7 +1,6 @@
 package com30022.server.twilio
 
-import com.twilio.jwt.accesstoken.AccessToken
-import com.twilio.jwt.accesstoken.ChatGrant
+import com.twilio.jwt.accesstoken.*
 
 private const val TWILIO_APP_NAME = "GUGUGU"
 private const val TWILIO_FIREBASE_PUSH_CREDENTIAL = "CR32258191da074808a5975001f0bdc79b"
@@ -11,31 +10,44 @@ private const val TWILIO_API_KEY = "SKaebd656fa23853424d9edf7ac792e8ef"
 private const val TWILIO_API_SECRET = "RFlJSI0ou4lboaiPyYo7RdL3qxZ6axem"
 private const val TWILIO_CHAT_SERVICE_SID = "ISf316b89375a04ec8ab22b3f7e6881625"
 
-private interface TokenGenerator {
-    operator fun invoke(uid: String, device: String): AccessToken
+private abstract class TokenGenerator protected constructor() {
+
+    abstract operator fun invoke(uid: String, device: String): AccessToken
+
+    fun buildToken(grant: Grant, uid: String): AccessToken {
+        return AccessToken.Builder(TWILIO_ACCOUNT_SID, TWILIO_API_KEY, TWILIO_API_SECRET).identity(uid).grant(grant)
+            .build()
+    }
 }
 
-private class ChatTokenGenerator : TokenGenerator {
+private class ChatTokenGenerator : TokenGenerator() {
     override operator fun invoke(uid: String, device: String): AccessToken {
         val grant = ChatGrant()
         grant.serviceSid = TWILIO_CHAT_SERVICE_SID
         grant.pushCredentialSid = TWILIO_FIREBASE_PUSH_CREDENTIAL
         grant.endpointId = "$TWILIO_APP_NAME:$uid:$device"
 
-        return AccessToken.Builder(TWILIO_ACCOUNT_SID, TWILIO_API_KEY, TWILIO_API_SECRET).identity(uid).grant(grant)
-            .build()
+        return buildToken(grant, uid)
     }
 }
 
-private class VoiceTokenGenerator : TokenGenerator {
+private class VoiceTokenGenerator : TokenGenerator() {
     override operator fun invoke(uid: String, device: String): AccessToken {
-        TODO("not implemented")
+        val grant = VoiceGrant()
+        // grant.setOutgoingApplication()
+        TODO("finalise implementation")
+
+        return buildToken(grant, uid)
     }
 }
 
-private class VideoTokenGenerator : TokenGenerator {
+private class VideoTokenGenerator : TokenGenerator() {
     override operator fun invoke(uid: String, device: String): AccessToken {
-        TODO("not implemented")
+        val grant = VideoGrant()
+        // grant.setOutgoingApplication()
+        TODO("finalise implementation")
+
+        return buildToken(grant, uid)
     }
 }
 
