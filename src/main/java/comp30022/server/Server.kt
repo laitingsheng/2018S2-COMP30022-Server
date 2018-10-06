6a944c97ddbcab4de1bf39e60c5631548e4635f4
Tinson <laitinson@gmail.com>
2018-10-06 13:51:34 +1000

    Separate token generation response

--- src/main/java/comp30022/server/Server.kt
+++ src/main/java/comp30022/server/Server.kt
@@ -8,6 +8,7 @@ import com.google.firebase.cloud.FirestoreClient
 import com.twilio.Twilio
 import com.twilio.jwt.accesstoken.AccessToken
 import com.twilio.jwt.accesstoken.ChatGrant
+import com.twilio.jwt.accesstoken.Grant
 import com.twilio.jwt.accesstoken.VideoGrant
 import com.twilio.rest.notify.v1.service.Binding
 import com.twilio.rest.notify.v1.service.Notification
@@ -25,33 +26,58 @@ private val LOGGER: Logger = Logger.getLogger(Server::class.java.name)
 private lateinit var USERS: CollectionReference
 private lateinit var CALLING: CollectionReference
 
+private fun buildToken(grant: Grant, identity: String): AccessToken {
+    return AccessToken.Builder(TWILIO_ACCOUNT_SID, TWILIO_API_KEY, TWILIO_API_SECRET).identity(identity).grant(grant)
+        .build()
+}
+
 @SpringBootApplication
 @RestController
 open class Server {
-    @RequestMapping(value = ["/twilio/token"], method = [RequestMethod.GET, RequestMethod.POST])
-    fun dispatchToken(type: String?, identity: String?, extra: String?): String? {
+    @RequestMapping(value = ["/twilio/register"], method = [RequestMethod.POST])
+    fun register(identity: String?, address: String?, tag: String?): String? {
+        return try {
+            if (identity!!.isEmpty() || address!!.isEmpty() || tag!!.isEmpty()) null else Binding.creator(
+                TWILIO_SERVICE_SID, identity, Binding.BindingType.FCM, address
+            ).setCredentialSid(TWILIO_FIREBASE_PUSH_CREDENTIAL).setTag(tag).create().sid
+        } catch (t: Throwable) {
+            LOGGER.log(Level.INFO, "Invalid request", t)
+            // any exceptions will return false represents invitation fail
+            null
+        }
+    }
+
+    @RequestMapping(value = ["/twilio/deregister"], method = [RequestMethod.POST])
+    fun deregister(sid: String?): Boolean {
+        return try {
+            Binding.deleter(TWILIO_SERVICE_SID, sid!!).delete()
+        } catch (e: Exception) {
+            LOGGER.log(Level.INFO, "Invalid request", e)
+            // any exceptions will return false represents invitation fail
+            false
+        }
+    }
+
+    @RequestMapping(value = ["/twilio/chat/token"], method = [RequestMethod.GET, RequestMethod.POST])
+    fun dispatchChatToken(identity: String?, extra: String?): String? {
         return try {
             // identity and extra should be non-null and non-empty
-            if (identity!!.isEmpty() || extra!!.isEmpty()) null else when (type) {
-                // generate chat grant
-                "chat" -> ChatGrant().setEndpointId("$TWILIO_APP_NAME:$identity:$extra").setPushCredentialSid(
+            if (identity!!.isEmpty() || extra!!.isEmpty()) null
+            else buildToken(
+                ChatGrant().setEndpointId("$TWILIO_APP_NAME:$identity:$extra").setPushCredentialSid(
                     TWILIO_FIREBASE_PUSH_CREDENTIAL
-                ).setServiceSid(TWILIO_CHAT_SERVICE_SID)
-                // generate video grant
-                "video" -> VideoGrant().setRoom(extra)
-                // throw an exception if type is not recognised
-                else -> throw AssertionError("invalid token type")
-            }.let {
-                // build the access token with the given non-null grant as well as the identity and convert it to JWT
-                // as the response
-                AccessToken.Builder(TWILIO_ACCOUNT_SID, TWILIO_API_KEY, TWILIO_API_SECRET).identity(identity).grant(it)
-                    .build().toJwt()
-            }
+                ).setServiceSid(TWILIO_CHAT_SERVICE_SID), identity
+            ).toJwt()
         } catch (t: Throwable) {
             LOGGER.log(Level.INFO, "invalid token request", t)
             // any exceptions will incur a response of null
             null
         }
+    }
+
+    @RequestMapping(value = ["/twilio/chat/token"], method = [RequestMethod.POST])
+    fun notifyMembers(): Boolean {
+        return false
     }
 
     @RequestMapping(value = ["/twilio/room/create"], method = [RequestMethod.POST])
@@ -81,46 +107,35 @@ open class Server {
         }
     }
 
-    @RequestMapping(value = ["/twilio/register"], method = [RequestMethod.POST])
-    fun register(identity: String?, address: String?, tag: String?): String? {
+    @RequestMapping(value = ["/twilio/call/token"], method = [RequestMethod.GET, RequestMethod.POST])
+    fun dispatchToken(identity: String?, extra: String?): String? {
         return try {
-            if (identity!!.isEmpty() || address!!.isEmpty()) null else Binding.creator(
-                TWILIO_SERVICE_SID, identity, Binding.BindingType.FCM, address
-            ).setCredentialSid(TWILIO_FIREBASE_PUSH_CREDENTIAL).setTag(tag ?: "default").create().sid
+            // identity and extra should be non-null and non-empty
+            if (identity!!.isEmpty() || extra!!.isEmpty()) null
+            else buildToken(VideoGrant().setRoom(extra), identity).toJwt()
         } catch (t: Throwable) {
-            LOGGER.log(Level.INFO, "Invalid request", t)
-            // any exceptions will return false represents invitation fail
+            LOGGER.log(Level.INFO, "invalid token request", t)
+            // any exceptions will incur a response of null
             null
-        }
-    }
-
-    @RequestMapping(value = ["/twilio/deregister"], method = [RequestMethod.POST])
-    fun deregister(sid: String?): Boolean {
-        return try {
-            Binding.deleter(TWILIO_SERVICE_SID, sid!!).delete()
-        } catch (e: Exception) {
-            LOGGER.log(Level.INFO, "Invalid request", e)
-            // any exceptions will return false represents invitation fail
-            false
         }
     }
 
     @RequestMapping(value = ["twilio/call/invite"], method = [RequestMethod.POST])
-    fun invite(identity: String?, roomSID: String?): String? {
+    fun invite(identity: String?, roomSID: String?): Boolean {
         return try {
-            if (identity!!.isEmpty() || roomSID!!.isEmpty()) null
-            else Notification.creator(TWILIO_SERVICE_SID).setIdentity(identity).setBody(roomSID).setPriority(
-                Notification.Priority.HIGH
-            ).create().sid
-        } catch (e: Exception) {
-            LOGGER.log(Level.INFO, "Invalid request", e)
+            if (identity!!.isEmpty() || roomSID!!.isEmpty()) false
+            else {
+                Notification.creator(TWILIO_SERVICE_SID).setIdentity(identity).setTag("video").setBody(roomSID)
+                    .setPriority(
+                        Notification.Priority.HIGH
+                    ).create()
+                true
+            }
+        } catch (t: Throwable) {
+            LOGGER.log(Level.INFO, "Invalid request", t)
             // any exceptions will return false represents invitation fail
-            null
+            false
         }
-    }
-
-    @RequestMapping(value = ["/twilio/room-status"], method = [RequestMethod.POST])
-    fun monitorRooms() {
     }
 }
 
