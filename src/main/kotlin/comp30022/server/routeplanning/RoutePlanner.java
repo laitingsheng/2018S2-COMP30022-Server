d3959e31612ee5c04962073d8665da2f300d566c
Raymond <1578942650@qq.com>
2018-10-17 16:42:44 +1100

    specify route method to be walking

--- src/main/kotlin/comp30022/server/routeplanning/RoutePlanner.java
+++ src/main/kotlin/comp30022/server/routeplanning/RoutePlanner.java
@@ -5,6 +5,7 @@ import com.google.maps.DirectionsApi;
 import com.google.maps.GeoApiContext;
 import com.google.maps.model.DirectionsResult;
 import com.google.maps.model.LatLng;
+import com.google.maps.model.TravelMode;
 import comp30022.server.util.Converter;
 
 import java.util.ArrayList;
@@ -25,6 +26,7 @@ public class RoutePlanner {
 
         DirectionsResult result = DirectionsApi
             .newRequest(geoApiContext)
+            .mode(TravelMode.WALKING)
             .origin(Converter.geoToLatLng(origins[0]))
             .destination(Converter.geoToLatLng(destinations[0]))
             .waypoints(wayPoints)
